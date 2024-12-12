190b44ba3e1a7eaff0b550eeb499a4ecd3679334
NarciSource <jwch11@gmail.com>
2024-12-12 21:51:58 +0900

    refactor: 알림 권한 요청 함수 async로 리팩토링

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-import registerPushNotifications from "./register_push_notifications.js";
+import register_push_notifications from "./register_push_notifications.js";
 
-function request_notification_permission() {
-    // 브라우저가 Notification API를 지원하는지 확인
+async function request_notification_permission() {
     if (!("Notification" in window)) {
         console.log("이 브라우저는 알림을 지원하지 않습니다.");
         return;
@@ -10,31 +9,22 @@ function request_notification_permission() {
     switch (Notification.permission) {
         case "granted":
             console.log("알림 권한이 허용되었습니다.");
-
-            registerPushNotifications();
             break;
         case "denied":
             console.log("알림 권한이 거부되었습니다.");
             break;
-        case "default":
+        default:
             console.log("알림 권한을 요청합니다.");
 
-            Notification.requestPermission()
-                .then((permission) => {
-                    if (permission === "granted") {
-                        console.log("알림 권한이 허용되었습니다.");
+            try {
+                await Notification.requestPermission();
+            } catch (error) {
+                console.error("알림 권한 요청 중 오류가 발생했습니다.", error);
+            }
+    }
 
-                        registerPushNotifications();
-                    } else if (permission === "denied") {
-                        console.log("알림 권한이 거부되었습니다.");
-                    } else {
-                        console.log("알림 권한 요청이 무시되었습니다.");
-                    }
-                })
-                .catch((error) => {
-                    console.error("알림 권한 요청 중 오류가 발생했습니다:", error);
-                });
-            break;
+    if (Notification.permission === "granted") {
+        register_push_notifications();
     }
 }
 
