c10c318759af769dbb3a603fbbecefd7fbd91bc0
NarciSource <jwch11@gmail.com>
2024-12-13 11:01:20 +0900

    refactor: OneSignal SDK 로드를 ESM으로 처리리

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-window.OneSignalDeferred = window.OneSignalDeferred || [];
+(async () => {
+    await import("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js");
 
-OneSignalDeferred.push(async function (OneSignal) {
-    await OneSignal.init({
-        appId: "9ee7de51-45a6-4639-9284-0ad224b0f726",
-        notifyButton: {
-            enable: true,
-        },
-        serviceWorkerPath: "./OneSignalSDKWorker.js",
+    window.OneSignalDeferred = window.OneSignalDeferred || [];
+
+    OneSignalDeferred.push(async function (OneSignal) {
+        await OneSignal.init({
+            appId: "9ee7de51-45a6-4639-9284-0ad224b0f726",
+            notifyButton: {
+                enable: true,
+            },
+            serviceWorkerPath: "./OneSignalSDKWorker.js",
+        });
+
+        await OneSignal.login("testuser");
     });
-
-    await OneSignal.login("testuser");
-});
+})();
