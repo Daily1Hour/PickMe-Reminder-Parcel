b179b9407ea00377e79f33933cddb94060232a3c
NarciSource <jwch11@gmail.com>
2024-12-13 10:10:22 +0900

    feat: OneSignal SDK 사용

---
+++ src/index.js
@@ -0,0 +1,11 @@
+window.OneSignalDeferred = window.OneSignalDeferred || [];
+
+OneSignalDeferred.push(async function (OneSignal) {
+    await OneSignal.init({
+        appId: "9ee7de51-45a6-4639-9284-0ad224b0f726",
+        notifyButton: {
+            enable: true,
+        },
+        serviceWorkerPath: "./OneSignalSDKWorker.js",
+    });
+});
